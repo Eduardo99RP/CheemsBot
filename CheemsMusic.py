dbfe2d857c3bdfd111fa12e3f4f635bb90ccd2ca
Juan Eduardo Rivera Perez <68937183+Eduardo99RP@users.noreply.github.com>
2024-06-09 17:17:04 -0600

    CheemsMusic

--- CheemsMusic.py
+++ CheemsMusic.py
@@ -3,46 +3,66 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 from pytube import YouTube
+from pytube import Search
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 import os
+import re
 from dotenv import load_dotenv
 
 load_dotenv()
 
+
 intents = discord.Intents.all()
 intents.message_content = True
+intents.voice_states = True  # Para recibir eventos de cambio de estado de voz
 
-bot = commands.Bot(command_prefix="!", intents=intents, description="Tengo ansiedad")
+bot = commands.Bot(command_prefix="!", intents=intents, description="Tengo ansiedad", help_command=None)
 
 queues = {}
+last_text_channels = {}  # Diccionario para almacenar el último canal de texto usado
+
+# Configura las credenciales de la API de Spotify
+client_id = os.environ.get("CLIENT_ID_SP")
+client_secret = os.environ.get("CLIENT_SECRECT")
+
+# Inicializa el cliente de autenticación
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
 
 @bot.event
 async def on_ready():
-    print("✅ El bot esta en linea")
+    await bot.change_presence(activity=discord.Activity(name="🎵 Temgo Amsiedad 🎧"))
+    print("✅ El bot está en línea")
+    
+@bot.event
+async def on_voice_state_update(member, before, after):
+    # Verificar si un usuario ha cambiado de estado de voz en un canal de voz
+    if member.bot:
+        return
 
-async def download_and_play_audio(ctx, url):
+    voice_client = discord.utils.get(bot.voice_clients, guild=member.guild)
+
+    if voice_client and voice_client.channel:
+        # Verificar si el canal está vacío (solo el bot queda en el canal)
+        if len(voice_client.channel.members) == 1 and voice_client.channel.members[0] == bot.user:
+            if voice_client.is_playing() or voice_client.is_paused():
+                voice_client.stop()
+            if member.guild.id in queues:
+                queues[member.guild.id].clear()
+            # Enviar el mensaje al último canal de texto utilizado
+            if member.guild.id in last_text_channels:
+                last_channel = last_text_channels[member.guild.id]
+                await last_channel.send("❌ Me desconecto, no hay nadie conectado ❌")
+            await voice_client.disconnect()
+
+async def stream_youtube_audio(ctx, url):
     try:
-        # Obtener el directorio actual del script
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-
-        # Cambiar al directorio del script
-        os.chdir(script_dir)
-
-        # Descargar el video de YouTube
         yt = YouTube(url)
-        video = yt.streams.get_highest_resolution()
-        video_filename = f"{ctx.guild.id}_video_descargado.mp4"
-        audio_filename = f"{ctx.guild.id}_audio_extraido.mp3"
-
-        # Verificar si el archivo de audio ya existe y eliminarlo si es necesario
-        if os.path.exists(audio_filename):
-            os.remove(audio_filename)
-
-        video.download(filename=video_filename)
-
-        # Extraer el audio del video
-        os.system(f'ffmpeg -y -i "{video_filename}" -vn -acodec libmp3lame "{audio_filename}"')
-
-        # Reproducir el audio en el canal de voz del autor
+        audio_stream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
+        audio_url = audio_stream.url
+        
         voice_channel = ctx.author.voice.channel
         voice_client = get(bot.voice_clients, guild=ctx.guild)
         if voice_client and voice_client.is_connected():
@@ -50,53 +70,169 @@ async def download_and_play_audio(ctx, url):
         else:
             voice_client = await voice_channel.connect()
 
-        voice_client.play(discord.FFmpegPCMAudio(audio_filename), after=lambda e: print('done', e))
-
-        # Esperar hasta que la reproducción del audio termine
-        while voice_client.is_playing():
-            await asyncio.sleep(1)
-
-        # Eliminar los archivos después de reproducir el audio
-        print("Archivos eliminados: ", video_filename, audio_filename)
-        os.remove(video_filename)
-        os.remove(audio_filename)
-
-        # Revisar si hay más elementos en la cola
-        if ctx.guild.id in queues and len(queues[ctx.guild.id]) > 0:
-            next_url = queues[ctx.guild.id].pop(0)
-            await download_and_play_audio(ctx, next_url)
+        ffmpeg_options = {
+            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -loglevel error',
+            'options': '-vn',
+            'stderr': open(os.devnull, 'w')
+        }
+        voice_client.play(discord.FFmpegPCMAudio(audio_url, **ffmpeg_options), after=lambda e: asyncio.run_coroutine_threadsafe(after_play(ctx), bot.loop))
 
     except Exception as e:
         await ctx.send(f"Ocurrió un error: {e}")
 
 @bot.command()
-async def play(ctx, *, url: str = None):
-    # Verifica si se proporciona una URL
-    if url is None:
-        await ctx.send("¡Debes proporcionar una URL para reproducir!")
-        return
+async def skip(ctx):
+    last_text_channels[ctx.guild.id] = ctx.channel  # Guardar el canal de texto
+    voice_client = get(bot.voice_clients, guild=ctx.guild)
+    if voice_client and voice_client.is_playing():
+        voice_client.stop()
+        await ctx.reply("⏭️ Canción saltada.")
 
+async def after_play(ctx):
+    voice_client = get(bot.voice_clients, guild=ctx.guild)
+    if voice_client and voice_client.is_connected() and not voice_client.is_playing():
+        if ctx.guild.id in queues and len(queues[ctx.guild.id]) > 0:
+            next_url = queues[ctx.guild.id].pop(0)
+            await stream_youtube_audio(ctx, next_url)
+                
+@bot.command()
+async def pause(ctx):
+    last_text_channels[ctx.guild.id] = ctx.channel  # Guardar el canal de texto
+    voice_client = get(bot.voice_clients, guild=ctx.guild)
+    if voice_client and voice_client.is_playing():
+        voice_client.pause()
+        await ctx.reply("⏸️ Canción en pausa.")
+    else:
+        await ctx.reply("❌ No hay nada reproduciendo actualmente para pausar.")
+
+@bot.command()
+async def resume(ctx):
+    last_text_channels[ctx.guild.id] = ctx.channel  # Guardar el canal de texto
+    voice_client = get(bot.voice_clients, guild=ctx.guild)
+    if voice_client and voice_client.is_paused():
+        voice_client.resume()
+        await ctx.reply("▶️ Reproducción reanudada.")
+    else:
+        await ctx.reply("❌ No hay ninguna canción en pausa para reanudar.")
+
+@bot.command()
+async def play(ctx, *, search_term: str = None):
+    last_text_channels[ctx.guild.id] = ctx.channel  # Guardar el canal de texto
+    url = None
     # Verifica si el autor del mensaje está en un canal de voz
     if ctx.author.voice:
-        # Verificar si hay una cola de reproducción para este servidor
-        if ctx.guild.id not in queues:
-            queues[ctx.guild.id] = []
+        # Verifica si se proporciona un término de búsqueda
+        if search_term:
+            # Verifica si el término de búsqueda es una URL de Spotify
+            if re.match(r'https?://(?:open\.)?spotify\.com/.+', search_term):
+                track_id = search_term.split('/')[-1].split('?')[0]
+                
+                track_info = sp.track(track_id)
 
-        # Agregar la URL a la cola de reproducción
-        queues[ctx.guild.id].append(url)
+                # Extrae el nombre de la pista y el nombre del artista
+                nombre_cancion = track_info['name']
+                nombre_artista = track_info['artists'][0]['name']
 
-        # Si no hay ningún audio reproduciéndose, iniciar la reproducción
-        if not ctx.voice_client or not ctx.voice_client.is_playing():
-            await download_and_play_audio(ctx, queues[ctx.guild.id].pop(0))
+                await ctx.send(f"Canción: {nombre_cancion}\nArtista: {nombre_artista}")
+                spot = Search(nombre_cancion + " " + nombre_artista)
+                results = spot.results
+                url = results[0].watch_url
+                
+                # Verificar si hay una cola de reproducción para este servidor
+                if ctx.guild.id not in queues:
+                    queues[ctx.guild.id] = []
+
+                # Agregar la URL a la cola de reproducción
+                queues[ctx.guild.id].append(url)
+
+                # Si no hay ningún audio reproduciéndose, iniciar la reproducción
+                if not ctx.voice_client or not ctx.voice_client.is_playing():
+                    await stream_youtube_audio(ctx, queues[ctx.guild.id].pop(0))
+                
+            else:
+                # Verifica si el término de búsqueda es una URL de YouTube
+                if re.match(r'https?://(?:www\.)?youtube\.com/.+', search_term) or re.match(r'https?://(?:www\.)?youtu\.be/.+', search_term):
+                    url = search_term
+                else:
+                    s = Search(search_term)
+                    results = s.results
+                    if results:
+                        url = results[0].watch_url
+                        await ctx.reply(f"**Canción a reproducir:**\n{url}")
+                    else:
+                        await ctx.reply("❌ No se encontraron resultados. ❌")
+                        return
+
+                # Verificar si hay una cola de reproducción para este servidor
+                if ctx.guild.id not in queues:
+                    queues[ctx.guild.id] = []
+
+                # Agregar la URL a la cola de reproducción
+                queues[ctx.guild.id].append(url)
+
+                # Si no hay ningún audio reproduciéndose, iniciar la reproducción
+                if not ctx.voice_client or not ctx.voice_client.is_playing():
+                    await stream_youtube_audio(ctx, queues[ctx.guild.id].pop(0))
+        else:
+            await ctx.reply("❌ Debes proporcionar una URL o un término de búsqueda. ❌")
     else:
-        await ctx.send("¡Debes estar en un canal de voz para usar este comando!")
+        await ctx.reply("❌ Necesitas estar en el canal de voz para reproducir música. ❌")
+
+@bot.command()
+async def stop(ctx):
+    last_text_channels[ctx.guild.id] = ctx.channel  # Guardar el canal de texto
+    voice_client = get(bot.voice_clients, guild=ctx.guild)
+    if voice_client:
+        if voice_client.is_playing() or voice_client.is_paused():
+            voice_client.stop()
+        if ctx.guild.id in queues:
+            queues[ctx.guild.id].clear()
+        await ctx.reply("🛑 Reproducción detenida y cola eliminada.")
+    else:
+        await ctx.reply("❌ No hay nada reproduciendo actualmente para detener.")
 
 @bot.command()
 async def q(ctx):
+    last_text_channels[ctx.guild.id] = ctx.channel  # Guardar el canal de texto
     if ctx.guild.id in queues and len(queues[ctx.guild.id]) > 0:
-        queue_content = "\n".join(queues[ctx.guild.id])
-        await ctx.send(f"**Cola de reproducción:**\n{queue_content}")
+        queue_content = []
+        for i, url in enumerate(queues[ctx.guild.id], start=1):
+            try:
+                yt = YouTube(url)
+                title = yt.title
+                queue_content.append(f"{i}. {title}")
+            except Exception as e:
+                queue_content.append(f"{i}. Error obteniendo título para {url}: {e}")
+        
+        queue_text = "\n".join(queue_content)
+        await ctx.reply(f"**Próximas canciones a reproducir:**\n`{queue_text}`")
     else:
-        await ctx.send("No hay elementos en la cola de reproducción.")
+        await ctx.reply("No hay elementos en la cola de reproducción.")
+
+@bot.command()
+async def help(ctx):
+    help_text = """
+    **Comandos disponibles:**
+    `!play <URL o término de búsqueda>`: Reproduce una canción de YouTube o Spotify.
+    `!skip`: Salta la canción actual.
+    `!pause`: Pausa la canción actual.
+    `!resume`: Reanuda la reproducción de la canción pausada.
+    `!stop`: Detiene la reproducción y limpia la cola.
+    `!q`: Muestra la cola de canciones.
+    `!help`: Muestra este mensaje de ayuda.
+    """
+    await ctx.send(help_text)
+
+#comando para hacer pruebas de codigo, este codigo no es necesario que este el bot
+@bot.command()
+async def ping(ctx):
+    await ctx.reply("pong")
+    
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.reply("❌ Ese comando no existe. Usa `!help` para ver la lista de comandos disponibles.")
+    else:
+        raise error
 
 bot.run(os.environ.get("TOKEN"))
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # CheemsBot
+Este bot utiliza varias bibliotecas de Python. A continuación se detalla cómo instalarlas.
 
+## Instalación
+
+Asegúrate de tener `pip` instalado y configurado correctamente. Puedes usar los siguientes comandos para instalar las dependencias necesarias:
+
+### Paquetes
+
+- **asyncio**: Esta biblioteca está incluida en la biblioteca estándar de Python (Python 3.4 o superior).
+- **discord.py**: Biblioteca para interactuar con la API de Discord.
+- **pytube**: Biblioteca para descargar videos de YouTube.
+- **spotipy**: Biblioteca para interactuar con la API de Spotify.
+- **python-dotenv**: Biblioteca para cargar variables de entorno desde un archivo `.env`.
+
+### Comandos de Instalación
+
+Ejecuta los siguientes comandos en tu terminal o línea de comandos:
+
+```sh
+pip install discord.py
+pip install pytube
+pip install spotipy
+pip install python-dotenv
+
+```
+### Archivo .env
+Asegúrate de crear un archivo **.env** en el directorio raíz del proyecto para almacenar tus variables de entorno.
+# .env example
+```sh
+TOKEN=El_token_del_bot
+CLIENT_ID_SP=CLiente_id_de_spotify
+CLIENT_SECRECT=Cliente_secreto_spotify
+```
+
+### Aviso
+
+Este bot está diseñado para uso personal y sin fines de lucro. No se busca hacer negocio con este proyecto ni generar ingresos a partir de su uso.
+
+**Responsabilidad del Usuario:** El uso de este bot es bajo la responsabilidad de quien lo utiliza. Los desarrolladores no se hacen responsables por cualquier daño o problema que pueda surgir del uso de este software.
+
+**Uso Acorde a las Políticas:** Asegúrate de utilizar este bot de acuerdo con las políticas y términos de servicio de las plataformas y servicios con los que interactúa (por ejemplo, Discord, YouTube, Spotify). No nos hacemos responsables por el uso indebido o violaciones a los términos de servicio de estas plataformas.
