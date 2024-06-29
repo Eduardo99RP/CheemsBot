310c3bc2d0cbbe782152002a120270c3435cfcb4
Juan Eduardo Rivera Perez <68937183+Eduardo99RP@users.noreply.github.com>
2024-06-29 17:10:08 -0600

    CheemsMusic

--- CheemsMusic.py
+++ CheemsMusic.py
@@ -2,67 +2,74 @@ import asyncio
 import discord
 from discord.ext import commands
 from discord.utils import get
-from pytube import YouTube
-from pytube import Search
+from pytube import YouTube, Search
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import os
 import re
 from dotenv import load_dotenv
 
+# Carga las variables de entorno del archivo .env
 load_dotenv()
 
-
+# Configuración de los intents de Discord
 intents = discord.Intents.all()
 intents.message_content = True
 intents.voice_states = True  # Para recibir eventos de cambio de estado de voz
 
-bot = commands.Bot(command_prefix="!", intents=intents, description="Tengo ansiedad", help_command=None)
+# Configuración del bot con prefijo de comando y descripción
+bot = commands.Bot(command_prefix="!", intents=intents,
+                   description="Tengo ansiedad", help_command=None)
 
-queues = {}
+queues = {}  # Diccionario para colas de música por servidor
 last_text_channels = {}  # Diccionario para almacenar el último canal de texto usado
 
 # Configura las credenciales de la API de Spotify
 client_id = os.environ.get("CLIENT_ID_SP")
 client_secret = os.environ.get("CLIENT_SECRECT")
 
-# Inicializa el cliente de autenticación
-client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+# Inicializa el cliente de autenticación de Spotify
+client_credentials_manager = SpotifyClientCredentials(
+    client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
 
 @bot.event
 async def on_ready():
+    # Evento que se ejecuta cuando el bot está listo
     await bot.change_presence(activity=discord.Activity(name="🎵 Temgo Amsiedad 🎧"))
     print("✅ El bot está en línea")
-    
+
 @bot.event
 async def on_voice_state_update(member, before, after):
-    # Verificar si un usuario ha cambiado de estado de voz en un canal de voz
+    # Evento al cambiar el estado de voz de un usuario
     if member.bot:
         return
 
+    # Obtiene el cliente de voz en la guild actual
     voice_client = discord.utils.get(bot.voice_clients, guild=member.guild)
 
     if voice_client and voice_client.channel:
-        # Verificar si el canal está vacío (solo el bot queda en el canal)
+        # Verificar si el canal está vacío (solo queda el bot)
         if len(voice_client.channel.members) == 1 and voice_client.channel.members[0] == bot.user:
             if voice_client.is_playing() or voice_client.is_paused():
-                voice_client.stop()
+                voice_client.stop()  # Detiene la reproducción
             if member.guild.id in queues:
-                queues[member.guild.id].clear()
-            # Enviar el mensaje al último canal de texto utilizado
+                queues[member.guild.id].clear()  # Limpia la cola de reproducción
+            # Enviar mensaje al último canal de texto utilizado
             if member.guild.id in last_text_channels:
                 last_channel = last_text_channels[member.guild.id]
                 await last_channel.send("❌ Me desconecto, no hay nadie conectado ❌")
-            await voice_client.disconnect()
+            await voice_client.disconnect()  # Desconecta el bot del canal de voz
 
 async def stream_youtube_audio(ctx, url):
+    # Función para transmitir audio de YouTube
     try:
         yt = YouTube(url)
+        # Obtiene el stream de audio de mayor bitrate
         audio_stream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
         audio_url = audio_stream.url
-        
+
+        # Conecta al canal de voz del usuario que ejecutó el comando
         voice_channel = ctx.author.voice.channel
         voice_client = get(bot.voice_clients, guild=ctx.guild)
         if voice_client and voice_client.is_connected():
@@ -72,83 +79,97 @@ async def stream_youtube_audio(ctx, url):
 
         ffmpeg_options = {
             'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -loglevel error',
-            'options': '-vn',
-            'stderr': open(os.devnull, 'w')
+            'options': '-vn',  # No video, solo audio
+            'stderr': open(os.devnull, 'w')  # Suprimir errores de FFMPEG
         }
-        voice_client.play(discord.FFmpegPCMAudio(audio_url, **ffmpeg_options), after=lambda e: asyncio.run_coroutine_threadsafe(after_play(ctx), bot.loop))
+        # Reproduce el audio utilizando FFMPEG
+        voice_client.play(discord.FFmpegPCMAudio(audio_url, **ffmpeg_options),
+                          after=lambda e: asyncio.run_coroutine_threadsafe(after_play(ctx), bot.loop))
 
     except Exception as e:
-        await ctx.send(f"Ocurrió un error: {e}")
+        await ctx.send(f"Ocurrió un error: {e}")  # Maneja errores de reproducción
 
 @bot.command()
-async def skip(ctx):
-    last_text_channels[ctx.guild.id] = ctx.channel  # Guardar el canal de texto
+async def skip(ctx, song_id: int = None):
+    # Comando para saltar la canción actual
+    last_text_channels[ctx.guild.id] = ctx.channel  # Guarda el canal de texto actual
     voice_client = get(bot.voice_clients, guild=ctx.guild)
+
+    # Si se proporciona un ID de canción, la elimina de la cola
+    if ctx.guild.id in queues and len(queues[ctx.guild.id]) > 0 and song_id is not None:
+        queues[ctx.guild.id].pop(song_id)
+
     if voice_client and voice_client.is_playing():
-        voice_client.stop()
-        await ctx.reply("⏭️ Canción saltada.")
+        voice_client.stop()  # Detiene la reproducción actual
+        await ctx.reply("⏭️ Canción saltada.")  # Informa al usuario
 
 async def after_play(ctx):
+    # Función que se llama después de la reproducción de una canción
     voice_client = get(bot.voice_clients, guild=ctx.guild)
     if voice_client and voice_client.is_connected() and not voice_client.is_playing():
+        # Si hay más canciones en la cola, reproduce la siguiente
         if ctx.guild.id in queues and len(queues[ctx.guild.id]) > 0:
             next_url = queues[ctx.guild.id].pop(0)
             await stream_youtube_audio(ctx, next_url)
-                
+
 @bot.command()
 async def pause(ctx):
-    last_text_channels[ctx.guild.id] = ctx.channel  # Guardar el canal de texto
+    # Comando para pausar la canción actual
+    last_text_channels[ctx.guild.id] = ctx.channel  # Guarda el canal de texto actual
     voice_client = get(bot.voice_clients, guild=ctx.guild)
     if voice_client and voice_client.is_playing():
-        voice_client.pause()
+        voice_client.pause()  # Pausa la reproducción
         await ctx.reply("⏸️ Canción en pausa.")
     else:
         await ctx.reply("❌ No hay nada reproduciendo actualmente para pausar.")
 
 @bot.command()
 async def resume(ctx):
-    last_text_channels[ctx.guild.id] = ctx.channel  # Guardar el canal de texto
+    # Comando para reanudar la canción pausada
+    last_text_channels[ctx.guild.id] = ctx.channel  # Guarda el canal de texto actual
     voice_client = get(bot.voice_clients, guild=ctx.guild)
     if voice_client and voice_client.is_paused():
-        voice_client.resume()
+        voice_client.resume()  # Reanuda la reproducción
         await ctx.reply("▶️ Reproducción reanudada.")
     else:
         await ctx.reply("❌ No hay ninguna canción en pausa para reanudar.")
 
 @bot.command()
 async def play(ctx, *, search_term: str = None):
-    last_text_channels[ctx.guild.id] = ctx.channel  # Guardar el canal de texto
+    # Comando para reproducir una canción de YouTube o Spotify
+    last_text_channels[ctx.guild.id] = ctx.channel  # Guarda el canal de texto actual
     url = None
     # Verifica si el autor del mensaje está en un canal de voz
     if ctx.author.voice:
-        # Verifica si se proporciona un término de búsqueda
         if search_term:
             # Verifica si el término de búsqueda es una URL de Spotify
             if re.match(r'https?://(?:open\.)?spotify\.com/.+', search_term):
                 track_id = search_term.split('/')[-1].split('?')[0]
-                
                 track_info = sp.track(track_id)
 
-                # Extrae el nombre de la pista y el nombre del artista
+                # Extrae el nombre de la pista y el artista
                 nombre_cancion = track_info['name']
                 nombre_artista = track_info['artists'][0]['name']
 
                 await ctx.send(f"Canción: {nombre_cancion}\nArtista: {nombre_artista}")
                 spot = Search(nombre_cancion + " " + nombre_artista)
                 results = spot.results
-                url = results[0].watch_url
-                
-                # Verificar si hay una cola de reproducción para este servidor
+
+                if results:
+                    url = results[0].watch_url
+                    await ctx.reply(f"**Canción a reproducir:**\n{url}")
+                else:
+                    await ctx.reply("❌ No se encontraron resultados. ❌")
+                    return
+
+                # Agrega la URL a la cola de reproducción
                 if ctx.guild.id not in queues:
                     queues[ctx.guild.id] = []
-
-                # Agregar la URL a la cola de reproducción
                 queues[ctx.guild.id].append(url)
 
-                # Si no hay ningún audio reproduciéndose, iniciar la reproducción
+                # Inicia la reproducción si no hay nada reproduciendo
                 if not ctx.voice_client or not ctx.voice_client.is_playing():
                     await stream_youtube_audio(ctx, queues[ctx.guild.id].pop(0))
-                
             else:
                 # Verifica si el término de búsqueda es una URL de YouTube
                 if re.match(r'https?://(?:www\.)?youtube\.com/.+', search_term) or re.match(r'https?://(?:www\.)?youtu\.be/.+', search_term):
@@ -163,14 +184,12 @@ async def play(ctx, *, search_term: str = None):
                         await ctx.reply("❌ No se encontraron resultados. ❌")
                         return
 
-                # Verificar si hay una cola de reproducción para este servidor
+                # Agrega la URL a la cola de reproducción
                 if ctx.guild.id not in queues:
                     queues[ctx.guild.id] = []
-
-                # Agregar la URL a la cola de reproducción
                 queues[ctx.guild.id].append(url)
 
-                # Si no hay ningún audio reproduciéndose, iniciar la reproducción
+                # Inicia la reproducción si no hay nada reproduciendo
                 if not ctx.voice_client or not ctx.voice_client.is_playing():
                     await stream_youtube_audio(ctx, queues[ctx.guild.id].pop(0))
         else:
@@ -180,30 +199,33 @@ async def play(ctx, *, search_term: str = None):
 
 @bot.command()
 async def stop(ctx):
-    last_text_channels[ctx.guild.id] = ctx.channel  # Guardar el canal de texto
+    # Comando para detener la reproducción y limpiar la cola
+    last_text_channels[ctx.guild.id] = ctx.channel  # Guarda el canal de texto actual
     voice_client = get(bot.voice_clients, guild=ctx.guild)
     if voice_client:
         if voice_client.is_playing() or voice_client.is_paused():
-            voice_client.stop()
+            voice_client.stop()  # Detiene la reproducción
         if ctx.guild.id in queues:
-            queues[ctx.guild.id].clear()
+            queues[ctx.guild.id].clear()  # Limpia la cola
         await ctx.reply("🛑 Reproducción detenida y cola eliminada.")
     else:
         await ctx.reply("❌ No hay nada reproduciendo actualmente para detener.")
 
 @bot.command()
 async def q(ctx):
-    last_text_channels[ctx.guild.id] = ctx.channel  # Guardar el canal de texto
+    # Comando para mostrar la cola de canciones
+    last_text_channels[ctx.guild.id] = ctx.channel  # Guarda el canal de texto actual
     if ctx.guild.id in queues and len(queues[ctx.guild.id]) > 0:
         queue_content = []
         for i, url in enumerate(queues[ctx.guild.id], start=1):
             try:
                 yt = YouTube(url)
-                title = yt.title
+                title = yt.title  # Obtiene el título del video
                 queue_content.append(f"{i}. {title}")
             except Exception as e:
-                queue_content.append(f"{i}. Error obteniendo título para {url}: {e}")
-        
+                queue_content.append(
+                    f"{i}. Error obteniendo título para {url}: {e}")
+
         queue_text = "\n".join(queue_content)
         await ctx.reply(f"**Próximas canciones a reproducir:**\n`{queue_text}`")
     else:
@@ -211,6 +233,7 @@ async def q(ctx):
 
 @bot.command()
 async def help(ctx):
+    # Comando para mostrar la lista de comandos disponibles
     help_text = """
     **Comandos disponibles:**
     `!play <URL o término de búsqueda>`: Reproduce una canción de YouTube o Spotify.
@@ -223,16 +246,19 @@ async def help(ctx):
     """
     await ctx.send(help_text)
 
-#comando para hacer pruebas de codigo, este codigo no es necesario que este el bot
 @bot.command()
 async def ping(ctx):
+    # Comando para probar la latencia del bot
     await ctx.reply("pong")
-    
+
 @bot.event
 async def on_command_error(ctx, error):
+    # Manejo de errores de comandos
     if isinstance(error, commands.CommandNotFound):
         await ctx.reply("❌ Ese comando no existe. Usa `!help` para ver la lista de comandos disponibles.")
     else:
         raise error
 
+# Ejecuta el bot con el token proporcionado en las variables de entorno
 bot.run(os.environ.get("TOKEN"))
+
